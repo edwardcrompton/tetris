423c1d18985797506e029737804643d68b69d2a8
Edward Crompton <edward.crompton@cameronandwilding.com>
2015-02-25 00:01:04 +0530

    Adding a grid that should keep track of the position of the shape as it moves.

--- public_html/tetris.js
+++ public_html/tetris.js
@@ -9,9 +9,8 @@ var RIGHT = 2;
 var BOTTOM = 3;
 
 var cellSide = 24;
-
-var stageWidth = cellSide * 18;
-var stageHeight = cellSide * 24;
+var gridCols = 18;
+var gridRows = 24;
 
 var stageOrigin = [0,0];
 var timeOutInterval = 500;
@@ -28,8 +27,36 @@ var tetrinosConfig = [
 // Create an new instance of a pixi stage
 var stage = new PIXI.Stage(0x000000);
 
+// Stage dimensions.
+var stageWidth = cellSide * gridCols;
+var stageHeight = cellSide * gridRows;
+
 // Create a renderer instance.
 var renderer = PIXI.autoDetectRenderer(stageWidth, stageHeight);
+
+var gameGrid = function() {  
+  // Initialise the grid.
+  this.grid = createArray(gridCols, gridRows);
+  this.oldActiveShapeCoors = new Array(0, 0);
+  
+  this.initialiseActiveShape = function (shapeCoors) {
+    // Map the active shape on to the grid.
+    for (i = 0; i < shapeCoors.length; i++) {
+      this.grid[stageOrigin[0] + shapeCoors[i][0]][stageOrigin[1] + shapeCoors[i][1]] = 1;
+    }
+  }
+  
+  this.moveActiveShape = function (x, y, shapeCoors) {
+    for (i = 0; i < shapeCoors.length; i++) {
+      // Wipe the old position of the active shape.
+      this.grid[stageOrigin[0] + this.oldActiveShapeCoors[0] + shapeCoors[i][0]][stageOrigin[1] + this.oldActiveShapeCoors[0] + shapeCoors[i][1]] = 0;
+      // Map the active shape on to the grid in its new position.
+      this.grid[stageOrigin[0] + x + shapeCoors[i][0]][stageOrigin[1] + y + shapeCoors[i][1]] = 1;
+    }
+    // Save the origin of the shape for next time.
+    this.oldActiveShapeCoors = [x, y];
+  }
+}
 
 var tetrino = function() {
   // Here's the constructor of the object.
@@ -39,8 +66,9 @@ var tetrino = function() {
   this.shapeRotation = Math.floor(Math.random() * (this.shapeConfig.length));
   this.shapeCoors = this.shapeConfig[this.shapeRotation];
   
-  this.xPos = 0;
-  this.yPos = 0;
+  // The cell coordinates of the shape's position on the grid.
+  this.x = 0;
+  this.y = 0;
   
   // Create a new graphics object
   this.graphics = new PIXI.Graphics();
@@ -56,8 +84,8 @@ var tetrino = function() {
 
       this.graphics.beginFill(0x00FF00);
       this.graphics.drawRect(
-        stageOrigin[0] + cellOrigin[0] + this.xPos, 
-        stageOrigin[1] + cellOrigin[1] + this.yPos,
+        stageOrigin[0] + cellOrigin[0] + (this.x * cellSide), 
+        stageOrigin[1] + cellOrigin[1] + (this.y * cellSide),
         cellSide, 
         cellSide
       );
@@ -71,18 +99,15 @@ var tetrino = function() {
     
     stage.addChild(piece.graphics);
 
+    // Update the shape on the grid.
+    grid.moveActiveShape(piece.x, piece.y, piece.shapeCoors);
+
     renderer.render(stage);
     
     var t = this;
     setTimeout(function () {
-      origPos = t.yPos;
-      if (!t.touchingEdge(BOTTOM)) {
-        t.yPos += cellSide;
-        t.fall();
-      }
-      else {
-        t.yPos = origPos;
-      }
+      t.y += 1;
+      t.fall();
     }, timeOutInterval);
   }
   
@@ -109,6 +134,18 @@ var tetrino = function() {
         return false;
     }
   }
+}
+
+function createArray(cols, rows) {
+  var a = new Array();
+
+  for (x = 0; x < cols; x++) {
+    a[x] = new Array();
+    for (y = 0; y < cols; y++) {
+      a[x][y] = 0;
+    }
+  }
+  return a;
 }
 
 function getEdgeCell(coordinates, edge) {
@@ -149,10 +186,16 @@ function getEdgeCell(coordinates, edge) {
 // Add the renderer view element to the DOM
 document.body.appendChild(renderer.view);
 
+// Create the grid that will hold the pieces.
+var grid = new gameGrid();
 // Create a new tetris piece.
 var piece = new tetrino();
 // Start its fall from the top of the stage.
 piece.fall(0);
+// Add the new piece to the grid.
+grid.initialiseActiveShape(piece.shapeCoors);
+
+console.log(grid.grid);
 
 // Event listener to listen for left, right, space keypresses.
 window.addEventListener('keydown', function(event) {
@@ -177,3 +220,9 @@ window.addEventListener('keydown', function(event) {
   }
   
 }, false);
+
+// Need to work out why the shape is sinking one level below the bottom of the
+// stage.
+// Also, after left and right keys have been pressed, movement speeds up.
+// Implement functionality for the space bar so that we can twist the shape.
+// Comment the functions and methods.
